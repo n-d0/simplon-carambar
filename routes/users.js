fe794fa05e1c082851993571dd8a14419231f25e
Nicolas Delage <nicolas@delage.co>
2025-07-28 00:04:13 +0200

    test sequelize

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
 
+const { Sequelize } = require('sequelize');
+
+// Option 2: Passing parameters separately (sqlite)
+const sequelize = new Sequelize({
+  dialect: 'sqlite',
+  storage: 'jokes.sqlite'
+});
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async function(req, res, next) {
+    let message;
+    try {
+        await sequelize.authenticate();
+        message = 'Connection has been established successfully.';
+    } catch (error) {
+        message = `Unable to connect to the database: ${error}`;
+    }
+    res.render('users', { message: message });
 });
 
 module.exports = router;
