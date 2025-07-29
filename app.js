057ce2aeb139cb32a68ffb79f9c8975071e41c51
Nicolas Delage <nicolas@delage.co>
2025-07-29 14:36:46 +0200

    add swagger documentation

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
 
 var indexRouter = require('./routes/index');
 var jokesRouter = require('./routes/api/jokes');
@@ -19,6 +21,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Swagger setup
+const swaggerOptions = {
+  swaggerDefinition: {
+    myapi: '3.0.0',
+    info: {
+      title: 'Carambar API',
+      version: '1.0.0',
+      description: 'API documentation',
+    },
+    servers: [
+      {
+        url: process.env.API_BASE_URL,
+      },
+    ],
+  },
+  apis: ['./routes/api/*.js'], // files containing annotations as above
+};
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/', indexRouter);
 app.use('/api/jokes', jokesRouter);
--- routes/api/jokes.js
+++ routes/api/jokes.js
@@ -3,7 +3,44 @@ var router = express.Router();
 const sequelize = require('../../config/sequelize');
 const Joke = require('../../models/Joke');
 
-/* GET jokes listing. */
+/**
+ * @swagger
+ * /api/jokes:
+ *   get:
+ *     summary: Retrieve a list of all jokes
+ *     responses:
+ *       200:
+ *         description: A list of jokes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     description: The ID of the joke.
+ *                     example: 1
+ *                   question:
+ *                     type: string
+ *                     description: The question part of the joke.
+ *                     example: Quelle est la femelle du hamster ?
+ *                   answer:
+ *                     type: string
+ *                     description: The answer part of the joke.
+ *                     example: L’Amsterdam
+ *                   createdAt:
+ *                     type: string
+ *                     format: date-time
+ *                     description: The date and time when the joke was created.
+ *                     example: 2025-07-28T08:52:39.248Z
+ *                   updatedAt:
+ *                     type: string
+ *                     format: date-time
+ *                     description: The date and time when the joke was last updated.
+ *                     example: 2025-07-28T08:52:39.248Z
+ */
 router.get('/', async function(req, res, next) {
     try {
         const jokes = await Joke.findAll();
@@ -13,7 +50,70 @@ router.get('/', async function(req, res, next) {
     }
 });
 
-/* POST endpoint to add a new joke */
+/**
+ * @swagger
+ * /api/jokes/add:
+ *   post:
+ *     summary: Add a new joke
+ *     description: Create a new joke with a question and an answer.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - question
+ *               - answer
+ *             properties:
+ *               question:
+ *                 type: string
+ *                 description: The question part of the joke.
+ *                 example: "Quelle est la femelle du hamster ?"
+ *               answer:
+ *                 type: string
+ *                 description: The answer part of the joke.
+ *                 example: "L'Amsterdam"
+ *     responses:
+ *       201:
+ *         description: The joke was created successfuly.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the joke.
+ *                   example: 1
+ *                 question:
+ *                   type: string
+ *                   description: The question part of the joke.
+ *                   example: "Quelle est la femelle du hamster ?"
+ *                 answer:
+ *                   type: string
+ *                   description: The answer part of the joke.
+ *                   example: "L'Amsterdam"
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Creation timestamp
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: Last update timestamp
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "Error: Internal Server Error"
+ */
 router.post('/add', async function(req, res, next) {
     try {
         await Joke.sync();
@@ -29,7 +129,54 @@ router.post('/add', async function(req, res, next) {
     }
 });
 
-/* GET a random joke */
+/**
+ * @swagger
+ * /api/jokes/random:
+ *   get:
+ *     summary: Retrieve a random joke
+ *     description: Fetch a random joke from the collection of jokes.
+ *     responses:
+ *       200:
+ *         description: A random joke.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the joke.
+ *                   example: 1
+ *                 question:
+ *                   type: string
+ *                   description: The question part of the joke.
+ *                   example: Quelle est la femelle du hamster ?
+ *                 answer:
+ *                   type: string
+ *                   description: The answer part of the joke.
+ *                   example: L’Amsterdam
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time when the joke was created.
+ *                   example: 2025-07-28T08:52:39.248Z
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time when the joke was last updated.
+ *                   example: 2025-07-28T08:52:39.248Z
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "Error: Internal Server Error"
+ */
 router.get('/random', async function(req, res, next) {
     try {
         const jokes = await Joke.findAll();
@@ -41,7 +188,61 @@ router.get('/random', async function(req, res, next) {
     }
 });
 
-/* GET joke by ID */
+/**
+ * @swagger
+ * /api/jokes/{id}:
+ *   get:
+ *     summary: Retrieve a joke by ID
+ *     description: Fetch a specific joke by its unique ID.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the joke to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A joke matching the given ID.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   description: The ID of the joke.
+ *                   example: 1
+ *                 question:
+ *                   type: string
+ *                   description: The question part of the joke.
+ *                   example: Quelle est la femelle du hamster ?
+ *                 answer:
+ *                   type: string
+ *                   description: The answer part of the joke.
+ *                   example: L’Amsterdam
+ *                 createdAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time when the joke was created.
+ *                   example: 2025-07-28T08:52:39.248Z
+ *                 updatedAt:
+ *                   type: string
+ *                   format: date-time
+ *                   description: The date and time when the joke was last updated.
+ *                   example: 2025-07-28T08:52:39.248Z
+ *       500:
+ *         description: Server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 error:
+ *                   type: string
+ *                   description: Error message.
+ *                   example: "Error: Internal Server Error"
+ */
 router.get('/:id', async function(req, res, next) {
     try {
         const jokeId = req.params.id;
