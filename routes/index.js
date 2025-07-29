67d5ac7f863c956c6dc5f8c4ae0bceb4f5683f2f
Nicolas Delage <nicolas@delage.co>
2025-07-29 09:59:28 +0200

    feature: landing page displays a random joke

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const fetch = require('node-fetch');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', async function(req, res, next) {
+    try {
+        const response = await fetch('http://localhost:3000/api/jokes/random');
+        const joke = await response.json();
+        res.render('index', { joke });
+    } catch (error) {
+        res.render('index', { joke: { question: error, answer: '' } });
+    }
 });
 
 module.exports = router;
